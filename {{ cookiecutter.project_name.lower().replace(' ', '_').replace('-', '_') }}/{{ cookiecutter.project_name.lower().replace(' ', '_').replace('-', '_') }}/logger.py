d6a54b82ecd5fa54c3eac3c4518d3b8070734208
Tomer Gabay <tomergabay@Admins-MacBook-Pro.local>
2023-01-06 15:06:00 +0100

    added default logger

--- {{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/{{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/logger.py
+++ {{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/{{ cookiecutter.project_name.lower().replace(' ', '_').replace('-', '_') }}/logger.py
@@ -1,6 +1,29 @@
+from types import TracebackType
+
 import logging
+import sys
 
 from rich.logging import RichHandler
+
+
+def log_unexpected_exception(
+    exc_type: type[BaseException], exc_value: BaseException, exc_traceback: TracebackType
+):
+    """Logs unexpected exceptions.
+
+    Args:
+        exc_type (type[BaseException]): exception type
+        exc_value (BaseException): value of the exception
+        exc_traceback (TracebackType): the traceback of the exception
+    """
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logger.critical("Uncaught exception!!", exc_info=(exc_type, exc_value, exc_traceback))
+
+
+sys.excepthook = log_unexpected_exception
 
 logger = logging.getLogger(__name__)
 
@@ -13,8 +36,8 @@ shell_handler.setLevel(logging.INFO)
 file_handler.setLevel(logging.DEBUG)
 
 # the formatter determines what our logs will look like
-fmt_shell = '%(message)s'
-fmt_file = '%(levelname)-8s %(asctime)s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s'
+fmt_shell = "%(message)s"
+fmt_file = "%(levelname)-8s %(asctime)s [%(filename)s:%(funcName)s:%(lineno)d] %(message)s"
 
 shell_formatter = logging.Formatter(fmt_shell)
 file_formatter = logging.Formatter(fmt_file)
